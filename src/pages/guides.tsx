48a8e1e8480f949f3ac5f0e12e0f1cd279da86f1
audieleon <audieleon@gmail.com>
2021-08-18 11:29:00 -0500

    Formatting changes.

--- src/pages/guides.tsx
+++ src/pages/guides.tsx
@@ -1,39 +1,37 @@
 import { StoplightProject } from "@stoplight/elements-dev-portal";
 import React from "react";
-import {Helmet} from "react-helmet"
+import { Helmet } from "react-helmet";
 import Layout from "../components/layout";
-import {getRedirect} from "../services/redirectTool"
+import { getRedirect } from "../services/redirectTool";
 
 require("@stoplight/elements-dev-portal/styles.min.css");
 
-const GuidesPage = ({location}) => {
+const GuidesPage = ({ location }) => {
   let redirect = "";
-  const nodeUri = location.pathname.split('/guides')[1];
+  const nodeUri = location.pathname.split("/guides")[1];
   const anchor = location.hash;
-  if (nodeUri.split("/").length > 2){
+  if (nodeUri.split("/").length > 2) {
     let path = getRedirect("/guides", nodeUri, anchor);
     redirect = path;
   }
- 
+
   return (
     <div>
-      { redirect !== "" ? (
+      {redirect !== "" ? (
         <Helmet>
-          <meta httpEquiv="refresh" 
-                content={`0;url=${redirect}`}/>
+          <meta httpEquiv="refresh" content={`0;url=${redirect}`} />
         </Helmet>
-        ) : (  
-          <Layout>   
-        <StoplightProject
-          platformUrl="https://stoplight.io"
-          projectId="cHJqOjk3NDQ"
-          basePath="guides"
-          collapseTableOfContents={true}
-          router={typeof window === "undefined" ? "memory" : "history"}
-        />
-        </Layout> 
-        )
-      }
+      ) : (
+        <Layout>
+          <StoplightProject
+            platformUrl="https://stoplight.io"
+            projectId="cHJqOjk3NDQ"
+            basePath="guides"
+            collapseTableOfContents={true}
+            router={typeof window === "undefined" ? "memory" : "history"}
+          />
+        </Layout>
+      )}
     </div>
   );
 };
