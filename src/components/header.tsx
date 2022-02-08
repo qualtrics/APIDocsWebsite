1e255c243583c01e3762a67f49f15ec470b1529d
Audie Leon Sheridan <audieleon@gmail.com>
2022-02-08 16:47:09 -0600

    Final Working Version, search and v1,v2 links work

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,8 +2,11 @@ import "../styles/qualtrics.css";
 import QualtricsLogo from "../images/QualtricsXM_WHITE.png";
 import { Link } from "gatsby";
 import React from "react";
+import { Search } from './Search';
+
 
 const Header = () => {
+
   return (
     <header className="blueBackground">
       <div className="flex baseline textCenter marginAuto headerWidth">
@@ -14,34 +17,35 @@ const Header = () => {
             </div>
           </Link>
 
-          <Link
-            to="/instructions/ZG9jOjg3NjY0Mg-overview"
+          <a
+            href="/ZG9jOjg3NjY0Mg-overview"
             className="reset textLarge headerMarginRight2"
           >
             <span className="textWhite">Instructions</span>
-          </Link>
+          </a>
 
-          <Link
-            to="/guides/ZG9jOjg3NjYzMQ-overview"
+          <a
+            href="/ZG9jOjg3NjYzMQ-overview"
             className="reset textLarge headerMarginRight2"
           >
             <span className="textWhite">Guides</span>
-          </Link>
+          </a>
 
-          <Link
-            to="/api-reference/ZG9jOjg0MDczOA-api-reference"
+          <a
+            href="/ZG9jOjg0MDczOA-api-reference"
             className="reset textLarge headerMarginRight2"
           >
             <span className="textWhite">API Reference</span>
-          </Link>
+          </a>
 
-          <Link
-            to="/sdks/ZG9jOjg3NjY0NQ-qualtrics-software-development-kits"
+          <a
+            href="/ZG9jOjg3NjY0NQ-qualtrics-software-development-kits"
             className="reset textLarge headerMarginRight2"
           >
             <span className="textWhite">SDKs</span>
-          </Link>
-
+          </a>
+          
+          <Search projectIds={['cHJqOjk3NDQ']} />
           {/* Search goes here. */}
         </div>
       </div>
